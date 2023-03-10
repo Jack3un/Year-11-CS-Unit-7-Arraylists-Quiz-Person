5958f4540efdddd33d97b394d9ed5a8069dd4c86
Jack3un <jacks2025@student.cis.edu.hk>
2023-03-10 08:59:11 +0800

    complete

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,49 @@
-public class Person {
-
+public class Person
+{
     private String firstName;
-    private String lastName;
+    private String surname;
     private int age;
-
 }
+
+public Person(String a, String b, int c)
+{
+    firstName = a;
+    surname = b;
+    age = c;
+}
+
+public String getFirstName()
+{
+    return firstName;
+}
+
+public void setFirstName(String firstName)
+{
+    this.firstName = firstName;
+}
+
+public String getSurname()
+{
+    return surname;
+}
+
+public void setSurname(String lastName)
+{
+    this.surname= lastName;
+}
+
+public int getAge()
+{
+    return age;
+}
+
+public void setAge(int age)
+{
+    this.age = age;
+}
+
+public String toString()
+{
+    return firstName + surname + age;
+}
+
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -1,22 +1,45 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class PersonFactory {
-
+public class PersonFactory
+{
     private ArrayList<Person> allPersons;
 
-    public PersonFactory() {
+    public PersonFactory()
+    {
         allPersons = new ArrayList<>();
     }
-
-    public void addPerson(Person person) {
-
+    public void addPerson(Person person)
+    {
+        int original_size = allPersons.size();
+        for(int i = 0; i < allPersons.size(); i++)
+        {
+            String string1 = person.getSurname();
+            String string2 = allPersons.get(i).getSurname();
+            if(string2.compareTo(string1 >= 0))
+                allPersons.add(i,person);
+        }
+        if(original_size == allPersons.size())
+            allPersons.add(person);
     }
-
-    public ArrayList<Person> under18() {
-
+    public ArrayList<Person> under18()
+        ArrayList<Person> u18List = new ArrayList<>();
+    for(Person person: allPersons)
+    {
+        if(person.getAge() < 18)
+            u18List.add(person);
     }
+    return u18List;
 
-    public boolean checkLastName(String lastName) {
-
+    public boolean checkSurname(String Surname)
+    {
+        for (Person person : allPersons)
+        {
+            if (person.getSurname().equals(Surname))
+                return true;
+        }
+        return false;
     }
 }
+
+
